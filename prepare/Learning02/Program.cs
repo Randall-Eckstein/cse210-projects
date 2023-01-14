435b40f857a7a575f4bdabb3c73996736b5b39d0
Randall-Eckstein <randalleckstein@yahoo.com>
2023-01-14 11:24:43 -0700

    Removed whitespace in program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -6,14 +6,9 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job("Better Butter Buyers", "Sales Assosciate", 1974, 1985);
-
-
         Job job2 = new Job("Tim's Duck Pond", "Senior Shooter", 1986, 0);
-
         List<Job> jobs = new List<Job>(){job1, job2};
-
         Resume resume = new Resume("Sexy Samantha", jobs);
-
         resume.Display();
     }
 }
