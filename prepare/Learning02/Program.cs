78172a3bfa95a06d8fa494226e5eed6e1dcd9474
Randall-Eckstein <randalleckstein@yahoo.com>
2023-01-14 11:26:29 -0700

    Adjusted name

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -8,7 +8,7 @@ class Program
         Job job1 = new Job("Better Butter Buyers", "Sales Assosciate", 1974, 1985);
         Job job2 = new Job("Tim's Duck Pond", "Senior Shooter", 1986, 0);
         List<Job> jobs = new List<Job>(){job1, job2};
-        Resume resume = new Resume("Sexy Samantha", jobs);
+        Resume resume = new Resume("Elvis Presley", jobs);
         resume.Display();
     }
 }
