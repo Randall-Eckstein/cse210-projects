a7c0166aec7b45c5bf6777b039f5079b8c2231e8
Randall-Eckstein <randalleckstein@yahoo.com>
2023-02-01 20:36:26 -0700

    Completed Fraction assignment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,23 @@
 using System;
+namespace Learning03;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction first = new Fraction();
+
+        Fraction second = new Fraction(12);
+
+        Fraction third = new Fraction(5, 8);
+        
+        Console.WriteLine(first.GetFractionString());
+        Console.WriteLine(first.GetDecimalValue());
+
+        Console.WriteLine(second.GetFractionString());
+        Console.WriteLine(second.GetDecimalValue());
+        
+        Console.WriteLine(third.GetFractionString());
+        Console.WriteLine(third.GetDecimalValue());
     }
 }
