93498e7b695cbfcfea98aed3bad9befe20fbb933
Randall-Eckstein <randalleckstein@yahoo.com>
2023-02-15 20:23:46 -0700

    Completed the Lesson4 Homework

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,19 @@
 using System;
+namespace Learning04;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+       MathAssignment math1 = new MathAssignment("Geometry", "6-12", "Samantha", "Mathematics");
+
+       WritingAssignment writing1 = new WritingAssignment("Diana", "Essays on World History", "Prevention of World War Two");
+
+       Console.WriteLine(math1.GetSummary());
+       Console.WriteLine(math1.GetHomeworkList());
+
+       Console.WriteLine(writing1.GetSummary());
+       Console.WriteLine(writing1.GetWritingInformation());
+       
     }
 }
