272e4092ba29d584879efed80a900b2f175ac7ac
Randall-Eckstein <randalleckstein@yahoo.com>
2023-03-24 16:38:44 -0600

    Added Save and Load classes

---
+++ final/FinalProject/Load.cs
@@ -0,0 +1,10 @@
+namespace FinalProject
+{
+    public class Load
+    {
+        public Load()
+        {
+            
+        }
+    }
+}
---
+++ final/FinalProject/Save.cs
@@ -0,0 +1,12 @@
+namespace FinalProject
+{
+    public class Save
+    {
+        private List<Media> _media;
+
+        public Save(List<Media> media)
+        {
+            this._media = media;
+        }
+    }
+}
